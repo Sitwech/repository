28bef0da3001e34607bb523a05f7c2454f2a2e65
Pablo Scandalo <pscandalo@gmail.com>
2019-06-06 15:18:13 -0700

    build new version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-export * from './src/Decorators';
-export * from './src/Types';
+export * from './src/decorators';
+export * from './src/types';
 export * from './src/baseRepository';
 export * from './src/mongoHelper';
